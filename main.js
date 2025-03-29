e4765593a26457613e1a7d63661786328bca7479
matheuspenas <mathegupena@gmail.com>
2025-03-28 22:14:03 -0300

    finalização do js

--- main.js
+++ main.js
@@ -3,4 +3,20 @@
 function colorirDia(){
     // bloco de variavveis
     let days = document.getElementById('day').value;
+    let color = document.getElementById('color').value;
+    let calendar = document.getElementById('calendar');
+
+    if(!days){
+        alert("Favor informe um dia existente no calendario");
+    }else{
+        if((days > 0) && (days<30)){
+            let td = calendar.getElementsByTagName('td'); 
+            td.style.backgroud-color == color;
+        }else{
+            alert("Favor informar uma data constante no calendario")
+        }
+    }
+
+    var elementos = document.querySelectorAll('td');
+
 }
